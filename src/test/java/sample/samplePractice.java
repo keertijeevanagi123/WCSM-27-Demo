9de11775ba1fe46979ca5e50fab0a0ece2c91700
keertijeevanagi <keerti019@gmail.com>
2023-04-14 11:20:04 +0530

    committed by branch34

--- src/test/java/sample/samplePractice.java
+++ src/test/java/sample/samplePractice.java
@@ -6,6 +6,7 @@ public class samplePractice {
 	@Test
 	public void test() {
 	System.out.println("sample is executed");
-
+	System.out.println("hi this is already executed");
+	System.out.println("hellooooooo this is already executed");
 }
 }
